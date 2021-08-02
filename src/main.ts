75cdb19f4c046ab6751d33e900b4fa6fd32fbf06
Dimitri Mostrey <dmostrey@yahoo.com>
2021-08-02 12:38:19 +0800

    project setup phase 1

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,39 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//import './plugins/axios'
+import "./assets/tailwind.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faAddressCard,
+  faAt,
+  faBookmark,
+  faEnvelope,
+  faExternalLinkSquareAlt,
+  faMapMarkedAlt,
+  faMapMarkerAlt,
+  faPhoneSquareAlt,
+  faUserSecret,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-createApp(App).use(store).use(router).mount('#app')
+library.add(
+  faAddressCard,
+  faAt,
+  faBookmark,
+  faEnvelope,
+  faExternalLinkSquareAlt,
+  faMapMarkedAlt,
+  faMapMarkerAlt,
+  faPhoneSquareAlt,
+  faUserSecret,
+);
+
+const app = createApp(App)
+  .use(store)
+  .use(router)
+  .component("font-awesome-icon", FontAwesomeIcon);
+//app.config.globalProperties.$_ = Lodash;
+app.config.globalProperties.apiUrl = "https://parrot.app/api/v1/";
+app.config.globalProperties.staticUrl = "https://static.puertoparrot.com/";
+app.mount("#app");
