2bbc9238785482d968ae13a874569a1eb0460473
Dimitri Mostrey <dmostrey@yahoo.com>
2021-08-12 20:21:20 +0800

    Pool store setup

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,32 @@
 import { createStore } from 'vuex'
+import PoolDataService from '@/services/PoolDataService'
 
 export default createStore({
-  state: {
+  state: () => ({
+    isAdmin: false,
+    loading: false,
+    season: '2019/07',
+    events: [],
+    dates: []
+  }),
+  actions: {
+    getCalendar({ commit }) {
+      return new Promise(() => {
+      PoolDataService("getCalendar")
+        .then((response: any) => {
+          console.log(response.data.data);
+          commit("updateDates", response.data.data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    });
+    }
   },
   mutations: {
-  },
-  actions: {
+      updateDates(state, payload) {
+        state.dates = payload;
+      }
   },
   modules: {
   }
