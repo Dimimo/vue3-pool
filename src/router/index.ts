9083257b88bd32fa5103ca28cb9cb9ac201f395e
Dimitri Mostrey <dmostrey@yahoo.com>
2021-08-17 20:12:24 +0800

    Calendar link

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,19 +3,29 @@ import Home from '../views/Home.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/calendar',
-    name: 'Calendar',
+    path: "/calendar",
+    name: "calendar",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "calendar" */ '../views/Calendar.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "calendar" */ "../views/Calendar.vue"),
+  },
+  {
+    path: "/results",
+    name: "results",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "results" */ "../views/Results.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
