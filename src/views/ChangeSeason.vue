325c3f391422475193618c0c4e5f84d97106656e
Dimitri Mostrey <dmostrey@yahoo.com>
2021-11-21 16:23:03 +0800

    change the season, reroute back

---
+++ src/views/ChangeSeason.vue
@@ -0,0 +1,24 @@
+<script>
+import { mapState } from "vuex";
+export default {
+  name: "ChangeSeason",
+  // eslint-disable-next-line
+  computed: mapState(["season"]),
+  methods: {
+    // eslint-disable-next-line
+    changeSeason() {
+      this.$store.dispatch("postSeason", this.season).then(() => {
+          console.log(this.$router.currentRoute.value.fullPath);
+        this.$router.back();
+      });
+    },
+  },
+  // eslint-disable-next-line
+  mounted() {
+    if (this.$route.params.year) {
+      this.season = this.$route.params.year + "/" + this.$route.params.month;
+      this.changeSeason();
+    }
+  },
+};
+</script>
