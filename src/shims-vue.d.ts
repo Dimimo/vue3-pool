7d1d3d36646973c05dae29d14ab97f5587bbd487
Dimitri Mostrey <dmostrey@yahoo.com>
2021-12-02 22:03:13 +0800

    store global for typescript

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,6 +1,26 @@
+import { Store } from "vuex";
+
 /* eslint-disable */
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
+
+declare module "vue/types/vue" {
+  interface Vue {
+    $router: VueRouter;
+  }
+}
+
+declare module "@vue/runtime-core" {
+  // declare your own store states
+  interface State {
+    count: number;
+  }
+
+  // provide typings for `this.$store`
+  interface ComponentCustomProperties {
+    $store: Store<State>;
+  }
 }
